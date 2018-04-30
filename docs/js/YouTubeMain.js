b7845947ba3f003a8c99672980bcbb3e804c677f
hyunyoung2 <hyun02.engineer@gmail.com>
2018-04-30 21:46:31 +0900

    setting

---
+++ docs/js/YouTubeMain.js
@@ -0,0 +1,21 @@
+ 
+
+
+// Dean Attali / Beautiful Jekyll 2016
+
+var main = {
+  inihttps://github.com/daattali/shinyjs/blob/gh-pages/index.mdt : function() {
+    // Shorten the navbar after scrolling a little bit down
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar").addClass("top-nav-short");
+      } else {
+        $(".navbar").removeClass("top-nav-short");
+      }
+    });
+  }
+};
+
+// 2fc73a3a967e97599c9763d05e564189
+
+document.addEventListener('DOMContentLoaded', main.init);
